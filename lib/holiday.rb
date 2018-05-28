703922a403719f7e5a449e1b785318d8ce030b18
Elaine Anderson <eparie@bu.edu>
2018-05-28 23:32:44 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -77,9 +77,10 @@ def all_supplies_in_holidays(holiday_hash)
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-holiday_hash.collect do
-if holiday_hash.value = "BBQ"
-  holiday_hash.key
+holiday_hash.collect do |season, holiday_hash|
+  holiday_hash.collect do |holiday, supplies|
+if supplies.include?("BBQ")
+  key
 end
 end
 end
