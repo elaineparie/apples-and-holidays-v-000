167c129d0f0f3438a62989ad4ae8c5850961c539
Elaine Anderson <eparie@bu.edu>
2018-05-28 22:06:39 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -63,8 +63,8 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holidays_hash|
     #{}"#{season}: #{holiday_hash}"
     holidays_hash.each do |holiday, supplies|
-    puts "#{season.capitalize}:
-    #{holiday}: #{supplies}"
+    puts "#{season.capitalize}:"
+    puts "#{holiday.capitalize}: #{supplies}"
     end
   end
 end
