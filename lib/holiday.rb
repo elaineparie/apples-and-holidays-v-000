1654349552030c039517f0da668856b135f679a9
Elaine Anderson <eparie@bu.edu>
2018-05-28 23:33:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,6 @@ def all_holidays_with_bbq(holiday_hash)
 holiday_hash.collect do |season, holiday_hash|
   holiday_hash.collect do |holiday, supplies|
 if supplies.include?("BBQ")
-  key
 end
 end
 end
