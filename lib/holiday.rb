9e267d67debc0b65654fee5565471a213d009cce
Elaine Anderson <eparie@bu.edu>
2018-05-28 23:29:52 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,4 +78,6 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
 
+if holiday_hash.value == "BBQ"
+  key
 end
